a9ccdc19ffe6a5e7d153fb4f2b4b5cc0758d0e4e
Thibaut Constant <thibaut.constant@isisphinx.com>
2019-06-01 01:48:34 +0200

    linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,31 +6,29 @@ import MuiLink from '@material-ui/core/Link'
 import ProTip from '../components/ProTip'
 import Link from '../components/Link'
 
-function MadeWithLove() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Built with love by the '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        Material-UI
-      </MuiLink>
-      {' team.'}
-    </Typography>
-  )
-}
+const MadeWithLove = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Built with love by the '}
+    <MuiLink color="inherit" href="https://material-ui.com/">
+      Material-UI
+    </MuiLink>
+    {' team.'}
+  </Typography>
+)
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Gatsby v4-beta example
-        </Typography>
-        <Link to="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <MadeWithLove />
-      </Box>
-    </Container>
-  )
-}
+const App = () => (
+  <Container maxWidth="sm">
+    <Box my={4}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Gatsby v4-beta example
+      </Typography>
+      <Link to="/about" color="secondary">
+        Go to the about page
+      </Link>
+      <ProTip />
+      <MadeWithLove />
+    </Box>
+  </Container>
+)
+
+export default App

538e540f95da8ae12293999fb0b23adda24826d9
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2019-06-05 22:48:35 +0200

    change link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import MuiLink from '@material-ui/core/Link'
 import { Link as GastsbyLink } from 'gatsby'
 
-const Link = React.forwardRef(function Link(props, ref) {
-  return <MuiLink component={GastsbyLink} ref={ref} {...props} />
-})
+const Link = (props: any) => {
+  return <MuiLink component={GastsbyLink} {...props} />
+}
 
 export default Link
